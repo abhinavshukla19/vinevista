da13d4a74732cd45b91d0ac375b6c74c1e241b3b
Abhinav <ayushabhinav19@gmail.com>
2025-11-22 20:53:50 +0530

    deploy

--- frontend/src/utils/toast.jsx
+++ frontend/src/utils/toast.jsx
@@ -20,7 +20,7 @@ export const ToastRoot = () => (
   />
 );
 
-export const host="http://192.168.1.7:3000"
+export const host="https://vinevista.onrender.com/"
 // export const host="http://34.47.254.72:3000"
 
 export const showSuccess = (message) => toast.success(message);
